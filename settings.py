2086c79b33bcb892f80d6647d02005b2df435729
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-01-24 16:07:25 +0100

    made game more balanced

--- settings.py
+++ settings.py
@@ -4,5 +4,6 @@ window_height = 1000
 
 food_num = 1000
 
-rounds_with_dangerous_snake = [100, 400, 600, 700, 800, 900]
-rounds_with_sticky_snake = [1000]
+a = [100 + round(0.05*(1.1**x)) for x in reversed(range(100))]
+rounds_with_dangerous_snake = [sum(a[:t]) for t in range(len(a))]
+rounds_with_sticky_snake = [2500, 5000, 10000]
